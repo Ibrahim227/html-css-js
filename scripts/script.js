1db56da167a94a1c82d152ab3937137f57a10377
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-08-03 21:39:04 +0100

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -27,3 +27,38 @@ function changeValue() {
     window.alert("Javascript changed this content after the Click!");
     console.log("Javascript changed this content after the Click!");
 }
+const person = {
+  fname: 'Mike',
+  lname: 'Lawson',
+  age: 46,
+  eyeColor: 'Blue',
+  jobStatus: 'Military',
+  country: 'illinois',
+  citizenship: 'USA',
+  life: {
+    currently: 'Jobless',
+    ownCar: 'No',
+    bankAccount: 'Bankrupt',
+    children: 2
+  }
+};
+document.getElementById("demo0").innerHTML = JSON.stringify(person);
+
+let text = " ";
+for (let x in person) {
+  text += person[x] + " ";
+}
+document.getElementById("demo1").innerHTML = JSON.stringify(text);
+
+const myArray = Object.values(person);
+
+document.getElementById("demo2").innerHTML = JSON.stringify(myArray);
+
+const fruits = {Bananas:300, Oranges:200, Apples:500};
+
+const txt = "";
+
+for (let [fruit, value] of Object.entries(fruits)) {
+  txt += fruit + ": " + value + '<br>';
+}
+document.getElementById("demo3").innerHTML = JSON.stringify(txt);
